3d41b0cc30f78411735074fa69b7be25a69b932d
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-02 10:57:10 +0000

    add twttr.py

---
+++ twttr/twttr.py
@@ -0,0 +1,34 @@
+def main():
+
+    str = input("Input: ")
+
+    str_without_vowels = remove_vowels(str)
+
+    print(f"Output: {str_without_vowels}")
+
+def remove_vowels(str):
+
+    vowels_list = [
+        "A", "E", "I", "O","U",
+        "a","e","i","o","u"
+    ]
+
+    char_list = []
+
+    for s in str:
+
+        # print(s)
+
+        if s not in vowels_list:
+
+            char_list.append(s)
+
+    # print(char_list)
+
+    result = ''.join(char_list)
+
+    return result
+
+
+
+main()
