0b564796ea2213eea14c1cd0f9ee47c1c591af37
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-02 10:23:20 +0000

    add

---
+++ bank/bank.py
@@ -0,0 +1,11 @@
+str = input("Greeting: ").strip()
+#print(str)
+
+if (str == "Hello" or str == "Hello, Newman"):
+    print("$0")
+
+elif (str == "How you doing?"):
+    print("$20")
+
+elif (str == "What's happening?" or str == "What's up?"):
+    print("$100")
