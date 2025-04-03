419e28dfc75743220bde25c74783b38c636568c0
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-03 14:14:47 +0000

    adieu.py: add the name_list variable to record name inputs

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,13 +1,17 @@
 def main():
 
+    name_list = []
+
     while True:
 
         try: 
             name = input("Name: ")
+            name_list.append(name)
 
         except EOFError:
             print()
-            print("Adieu, goodbye, auf Wiederseh'n")
+            print("Adieu, goodbye, auf Wiederseh'n, Die Zeit mit dir war schön")
+            print(name_list)
             break
 
 main()
