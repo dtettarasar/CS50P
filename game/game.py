360db0750fbf72feb472658f5ae699e35d4d15e6
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-03 15:11:36 +0000

    game.py: build the get_reult function

--- game/game.py
+++ game/game.py
@@ -14,6 +14,32 @@ def main():
 
     number_to_guess = random.randint(1, int(level))
 
-    print(f"number to guess: {number_to_guess}") 
+    print(f"number to guess: {number_to_guess}")
+
+    result = False
+    user_guess = ""
+
+    while user_guess.isdigit() == False:
+
+        user_guess = input("Guess: ")
+    
+    get_result(number_to_guess, int(user_guess))
+
+
+
+def get_result(number_to_guess, user_guess):
+
+    if user_guess < number_to_guess:
+        print("Too small!")
+        return False
+    
+    elif user_guess > number_to_guess:
+        print("Too large!")
+        return False
+    
+    else:
+        print('Just right!')
+        return True 
+
 
 main()
