fc87c7ce26c5ca691ee21e85368de06489993773
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-03 14:33:42 +0000

    adieu.py: adjustments on print instructions

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -13,12 +13,13 @@ def main():
             name_list.append(name)
 
         except EOFError:
+            
             print()
-            print("Adieu, goodbye, auf Wiederseh'n, Die Zeit mit dir war schön")
-            print(name_list)
+            # print("Adieu, goodbye, auf Wiederseh'n, Die Zeit mit dir war schön")
+            # print(name_list)
 
             lastStr = p.join(name_list , ",")
-            print(lastStr)
+            print(f'Adieu, adieu, to {lastStr}')
 
             break
 
