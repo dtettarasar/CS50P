78fa9e07e535b8545b50407655952a79378266bb
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-10 10:19:44 +0000

    bitcoin: build the api call

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,10 +1,14 @@
+# https://cs50.harvard.edu/python/2022/psets/4/bitcoin/
+
 import sys
+import requests
 
 def main():
 
     qty = get_arg()
 
-    print(qty)
+    get_price_index(qty)
+
 
 def get_arg():
 
@@ -29,5 +33,25 @@ def get_arg():
         except ValueError:
 
             sys.exit("Command-line argument is not a number")
+
+
+def get_price_index(qty):
+
+    print("init get price index")
+
+    print(qty)
+
+    r = None
+
+    try:
+
+        r = requests.get('https://rest.coincap.io/v3/assets/bitcoin?apiKey=ca296c840c2b68d060a5e503474ec1540aa8535ca5cd9112b575567f61523155')
+
+    except requests.RequestException:
+
+        sys.exit("Error with the API Call")
+
+    print(r.json())
+
 
 main()
