1a9455fc2f6d551b5581c589e5462206779c495b
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-10 09:56:56 +0000

    bitcoin: make sure the command line argument can be converted to float

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,16 +2,32 @@ import sys
 
 def main():
 
-    get_arg()
+    qty = get_arg()
+
+    print(qty)
 
 def get_arg():
 
     arg = sys.argv
 
-    print(arg)
+    # print(arg)
 
     if len(arg) == 1:
 
         sys.exit('Missing command-line argument')
+
+    elif len(arg) != 2:
+
+        sys.exit("You should provide only one command-line argument")
+
+    else:
+
+        try:
+
+            return float(arg[1])
+
+        except ValueError:
+
+            sys.exit("Command-line argument is not a number")
 
 main()
