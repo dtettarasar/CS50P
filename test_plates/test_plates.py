63059785a976e54e3be0a866346c26e5b639adc7
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-13 11:36:45 +0000

    building test for plates.py

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -3,3 +3,11 @@ from plates import is_valid
 def test_valid_plate():
 
     assert is_valid("CS50") == True
+
+def test_no_alphabectical_start():
+
+    assert is_valid("12") == False
+
+def test_wrong_length():
+
+    assert is_valid("H") == False
