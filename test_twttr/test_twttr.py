b2941f53233d7ed5eb88d23012ef9a387bea0d71
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-13 10:18:03 +0000

    test twttr : add test on punctuations and numbers

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -9,7 +9,14 @@ def test_uppercase():
 
     assert shorten("KORN") == "KRN"
 
-    
+def test_punctuations():
+
+    assert shorten("What's your name?") == "Wht's yr nm?"
+
+def test_numbers():
+
+    assert shorten("This Was CS50") == "Ths Ws CS50"
+
 if __name__ == "__main__":
     main()
 
