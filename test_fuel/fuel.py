2b4c1fadb3c36e8a1944cbb48c3b05cdd22f5060
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-14 15:31:44 +0000

    tets fuel: building new version of fuel.py

---
+++ test_fuel/fuel.py
@@ -0,0 +1,21 @@
+def main():
+    
+    fraction = False
+
+    while fraction == False:
+
+        str = input('Fraction: ')
+
+        fraction = convert(str)
+
+
+def convert(fraction):
+    return False
+
+
+def gauge(percentage):
+    ...
+
+
+if __name__ == "__main__":
+    main()
