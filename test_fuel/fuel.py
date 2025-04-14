e0c2f13eabe7649182eb73df7d24f9c2463f69d4
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-14 15:36:01 +0000

    fuel.py: building convert function

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -8,9 +8,47 @@ def main():
 
         fraction = convert(str)
 
+    print(fraction)
 
-def convert(fraction):
-    return False
+
+def convert(str):
+
+    fraction = {
+        "dividend": None,
+        "divisor": None
+    }
+
+    # To make sure the string match the fraction format
+    slash_check = str.count('/')
+
+    str_list = None
+
+    if slash_check == 1:
+
+        str_list = str.split('/')
+
+    else:
+
+        return False
+
+    fraction["dividend"] = str_list[0]
+    fraction["divisor"] = str_list[1]
+
+    # try to convert the list parameters to int
+    try:
+
+        fraction["dividend"] = int(fraction["dividend"])
+        fraction["divisor"] = int(fraction["divisor"])
+
+    except ValueError:
+
+        # print("values not valid")
+
+        return False
+
+    else:
+
+        return fraction
 
 
 def gauge(percentage):
