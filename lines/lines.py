2b00bbd8c5d08e3364331d45fda0ab046883af88
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-15 12:26:58 +0200

    lines.py build the get_file content function

--- lines/lines.py
+++ lines/lines.py
@@ -3,10 +3,17 @@ import sys
 def main():
 
     file_name = get_arg()
+    
+    file_content = get_file_content(file_name)
+    
+    for line in file_content:
+        
+        print(line)
+    
 
 def get_arg():
 
-    print('get arg function')
+    # print('get arg function')
     
     arg_list = sys.argv
     
@@ -21,10 +28,37 @@ def get_arg():
         sys.exit('Too many command-line arguments')
         
     file_name = arg_list[1]
-    print(file_name)
+    
+    # print(file_name)
     
     if file_name.endswith(".py") == False:
         
         sys.exit('Not a Python file')
+    
+    else:
+        
+        return file_name
+
+def get_file_content(file_name):
+    
+    print('init get file content')
+    print(file_name)
+    
+    try:
+        
+        with open(file_name, 'r') as f:
+            
+            lines = f.readlines()
+            
+    except FileNotFoundError:
+        
+        sys.exit('File does not exist')
+        
+    output = []
+    
+    for line in lines:
+        output.append(line)
+
+    return output
 
 main()
