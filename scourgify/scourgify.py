90f21627ccd07e5ebdf2fa65c8c19a92758447bf
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-17 11:14:52 +0200

    scourgify: building the get file names function

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -5,9 +5,26 @@ def main():
     
     print("init main function")
     
+    file_name = get_file_names()
+    
 def get_file_names():
     
     print("init get_file_names function")
+    
+    file_names = {
+        'input': None,
+        'output': None
+    }
+    
+    arg_list = sys.argv
+    
+    if len(arg_list) == 1:
+        
+        sys.exit('Too few command-line arguments')
+        
+    elif len(arg_list) > 3:
+        
+        sys.exit('Too many command-line arguments')
     
 def get_file_content(file_name): 
     
