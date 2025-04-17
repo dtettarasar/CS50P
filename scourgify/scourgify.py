191ce66a7d918e1fedbda0cfc00e9d39aaf9245b
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-17 11:09:56 +0200

    building scourgify.py

---
+++ scourgify/scourgify.py
@@ -0,0 +1,20 @@
+import sys
+import csv
+
+def main():
+    
+    print("init main function")
+    
+def get_file_names():
+    
+    print("init get_file_names function")
+    
+def get_file_content(file_name): 
+    
+    print("init get_file_content function")
+    
+def build_output_file(file_content):
+    
+    print("init build_output_file function")
+
+main()
