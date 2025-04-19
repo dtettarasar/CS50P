856803600a26b4ed0bfcf0743345fc4c2f249494
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-19 11:10:17 +0200

    shirt: building get file names function

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -4,4 +4,19 @@ def main():
 
 	print('init main function')
 
+	file_names = get_file_names()
+
+def get_file_names():
+
+	print('init get file names function')
+
+	file_names = {
+		
+		'input': None,
+		'ouput': None
+	
+	}
+
+	print(file_names)
+
 main()
