049420b856fa059100de5d453486a0e2a7d8bd46
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-19 11:52:47 +0200

    add conditions to check file names

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,22 +1,75 @@
 import sys
 
 def main():
-
-	print('init main function')
-
-	file_names = get_file_names()
-
+    
+    file_names = get_file_names()
+    
 def get_file_names():
+    
+    # print("init get_file_names function")
+    
+    valid_format = [
+        'jpg',
+        'jpeg',
+        'png'
+    ]
+    
+    file_names = {
+        'input': None,
+        'output': None
+    }
+    
+    arg_list = sys.argv
+    
+    if len(arg_list) <= 2:
+        
+        sys.exit('Too few command-line arguments')
+        
+    elif len(arg_list) > 3:
+        
+        sys.exit('Too many command-line arguments')
+     
+    file_names['input'] = arg_list[1]
+    file_names['output'] = arg_list[2]
+    
+    print(file_names)
+    
+    input_format = get_format(file_names['input'])
+    output_format = get_format(file_names['output'])
+    
+    print(f"input_format: {input_format}")
+    print(f"output_format: {output_format}")
+    
+    if input_format == False or input_format not in valid_format:
+        
+        sys.exit('Invalid input')
+        
+    if output_format == False or output_format not in valid_format:
+        
+        sys.exit('Invalid output')
+    
+    if input_format != output_format:
+        
+        sys.exit('Input and output have different extensions')
+        
+    
+# check that str contain one dot. if yes then return what's after the dot to get the file format
+def get_format(str):
 
-	print('init get file names function')
+    # str.count(".") == 1:
 
-	file_names = {
-		
-		'input': None,
-		'ouput': None
-	
-	}
+    if str.count(".") == 1:
+        # print("String format is valid")
+        format = str.split(".")
+        return format[1]
 
-	print(file_names)
+    elif str.count(".") == 2:
+        # print("String format is valid")
+        format = str.split(".")
+        return format[2]
 
+    else:
+        # print("String format is not valid")
+        return False
+    
 main()
