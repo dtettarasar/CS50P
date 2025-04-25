b163b40c833c3788406192b3ade0889f8fbfa4e6
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-25 13:07:31 +0200

    watch.py : building the regex to get the src attribute

--- watch/watch.py
+++ watch/watch.py
@@ -8,7 +8,19 @@ def main():
 
 def parse(s):
     
-    return check_iframe_format(s)
+    is_iframe =  check_iframe_format(s)
+    
+    if is_iframe == False:
+        
+        sys.exit("make sure you provide an iframe HTML element")
+        
+    # print(s)
+    
+    src_regex = 'src=\"https?:\/\/w?w?w?\.?youtube.com\/embed\/\w+\"'
+    
+    x = re.search(src_regex, s)
+    
+    print(x)
 
 def check_iframe_format(str):
 
