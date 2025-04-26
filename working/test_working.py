9bd70d918baa50da55a1e44164e4256a6fa15881
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-26 19:09:24 +0200

    working: build the initial files

---
+++ working/test_working.py
@@ -0,0 +1,5 @@
+from working import check_input
+
+def test_valid_input():
+    
+    assert check_input("9 AM to 5 PM") == True
---
+++ working/working.py
@@ -0,0 +1,20 @@
+import re
+import sys
+
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    ...
+
+
+...
+
+def check_input(str):
+    
+    return True
+
+if __name__ == "__main__":
+    main()
