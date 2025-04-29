68a777f7061cfa93fe0797ecc8317a7faf4811d9
Dylan Tettarasar <dtettarasar@gmail.com>
2025-04-29 16:29:48 +0200

    build response.py

--- response/response.py
+++ response/response.py
@@ -1,8 +1,19 @@
+import validators
+
 def main():
     print(verify_email(input("What's your email address? ")))
 
 def verify_email(str):
-    return True
+    
+    check_email = validators.email(str)
+    
+    if check_email == True:
+        
+        return "Valid"
+    
+    else:
+        
+        return "Invalid"
     
 if __name__ == "__main__":
     main()
