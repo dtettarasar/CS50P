1e8476d6aac25cf2881deb30fba997dd537f15e6
Dylan Tettarasar <dtettarasar@gmail.com>
2025-06-02 19:00:47 +0200

    shirtificate: adjust size and position for the pdf title

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -5,7 +5,7 @@ def main():
     pdf = FPDF(orientation="portrait", format="A4")
     pdf.add_page()
     pdf.set_font('helvetica', size=48)
-    pdf.cell(center=True,text="CS50 Shirtificate", h=56.5)
+    pdf.cell(center=True,text="CS50 Shirtificate", h=57)
     #pdf.image('shirtificate.png', w=120,h=252,x="C",keep_aspect_ratio=True,)
     pdf.output("shirtificate.pdf")
     
