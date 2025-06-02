225f42153a71795c8e37f4a7df412e36bfe95712
Dylan Tettarasar <dtettarasar@gmail.com>
2025-06-02 10:28:49 +0200

    add gitignore to exclude pdf files

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,4 +1,12 @@
+from fpdf import FPDF
+
 def main():
+    
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('helvetica', size=12)
+    pdf.cell(text="CS50 Shirtificate")
+    pdf.output("shirtificate.pdf")
     
     name = input('Name: ')
     
