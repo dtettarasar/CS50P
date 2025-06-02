9e028fcf1562855b4d431a8881d234dcd1efca84
Dylan Tettarasar <dtettarasar@gmail.com>
2025-06-02 19:10:09 +0200

    shirtificate: add the image

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,11 +6,11 @@ def main():
     pdf.add_page()
     pdf.set_font('helvetica', size=48)
     pdf.cell(center=True,text="CS50 Shirtificate", h=57)
-    #pdf.image('shirtificate.png', w=120,h=252,x="C",keep_aspect_ratio=True,)
+    pdf.image('shirtificate.png', w=190,x="C", y=70,keep_aspect_ratio=True,)
     pdf.output("shirtificate.pdf")
     
-    name = input('Name: ')
+    #name = input('Name: ')
     
-    print(f"your name is: {name}")
+    # print(f"your name is: {name}")
     
 main()
