a772d2fc901cc8f01ae8316d8197f16333a644b9
Dylan Tettarasar <dtettarasar@gmail.com>
2025-06-02 19:20:19 +0200

    shirtficate: build the text on the shirt

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -7,10 +7,16 @@ def main():
     pdf.set_font('helvetica', size=48)
     pdf.cell(center=True,text="CS50 Shirtificate", h=57)
     pdf.image('shirtificate.png', w=190,x="C", y=70,keep_aspect_ratio=True,)
+    
+    
+    pdf.set_font('helvetica', size=24)
+    pdf.set_text_color(255, 255, 255)
+    
+    name = input('Name: ')
+    
+    pdf.cell(center=True,text=f"{name} took CS50", h=252)
+    
     pdf.output("shirtificate.pdf")
     
-    #name = input('Name: ')
-    
-    # print(f"your name is: {name}")
     
 main()
