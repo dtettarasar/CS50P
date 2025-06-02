e8f0c281417c368d1876437f0e2a7e809f73f98c
Dylan Tettarasar <dtettarasar@gmail.com>
2025-06-02 11:01:06 +0200

    shirtificate: center the title

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,10 +2,10 @@ from fpdf import FPDF
 
 def main():
     
-    pdf = FPDF()
+    pdf = FPDF(orientation="portrait", format="A4")
     pdf.add_page()
     pdf.set_font('helvetica', size=12)
-    pdf.cell(text="CS50 Shirtificate")
+    pdf.cell(center=True,text="CS50 Shirtificate")
     pdf.output("shirtificate.pdf")
     
     name = input('Name: ')
